6982099d68173ef430176581c153ecafd35ded9b
Alec Chaprak <achaprak@gmail.com>
2023-12-27 14:52:32 -0800

    Removing React project to build it later

--- reacttoyauction/src/activeNavbar.js
+++
@@ -1,8 +0,0 @@
-const currentLocation = window.location.href;
-const menuItem = document.querySelectorAll('a.nav-link');
-for (let i = 0; i < menuItem.length; i++) {
-    if(menuItem[i].href === currentLocation) {
-        menuItem[i].className += " active";
-    }
-}
-
